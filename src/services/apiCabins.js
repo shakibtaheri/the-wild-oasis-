bf5ce020f6c6036755adcf39e79d59697a7adba9
shakibtaheri <shakibtaheri7@gmail.com>
2026-06-23 01:06:45 +0330

    complete cabins edit feature

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -19,19 +19,34 @@ export async function deleteCabin(id) {
   }
 }
 
-export async function createCabin(newCabin) {
+export async function createEditCabin(newCabin, id) {
+  const hasImagePath = newCabin.image?.startsWith?.(
+    "https://rxtuyjikrvagqaguiggu.supabase.co",
+  );
+
   const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
     "/",
     "",
   );
 
-  const imagePath = `https://rxtuyjikrvagqaguiggu.supabase.co/storage/v1/object/public/cabin-images/${imageName}`;
+  const imagePath = hasImagePath
+    ? newCabin.image
+    : `https://rxtuyjikrvagqaguiggu.supabase.co/storage/v1/object/public/cabin-images/${imageName}`;
 
-  // 1. create cabin
-  const { data, error } = await supabase
-    .from("cabins")
-    .insert([{ ...newCabin, image: imagePath }])
-    .select();
+  // 1. create/eadit cabin
+  let query = supabase.from("cabins");
+
+  // Create
+  if (!id) query = query.insert([{ ...newCabin, image: imagePath }]);
+
+  // Edit
+  if (id)
+    query = query
+      .update({ ...newCabin, image: imagePath })
+      .eq("id", id)
+      .select();
+
+  const { data, error } = await query.select().single();
 
   if (error) {
     console.error(error);
