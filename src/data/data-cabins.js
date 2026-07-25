adf8ec86b318fbe6f400b91dda4b35c5fa89b1e0
shakibtaheri <shakibtaheri7@gmail.com>
2026-07-26 01:01:17 +0330

    add uploader to siderbar,to update supabase data

--- src/data/data-cabins.js
+++ src/data/data-cabins.js
@@ -1,5 +1,4 @@
-import { supabaseUrl } from "../services/supabase";
-
+const supabaseUrl = "https://rxtuyjikrvagqaguiggu.supabase.co";
 const imageUrl = `${supabaseUrl}/storage/v1/object/public/cabin-images/`;
 
 export const cabins = [
